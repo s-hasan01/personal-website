0da29b5b69b8750f9e5b21a6989adee70e422cb2
Sajjad Hassan <sajjad.hasan074@gmail.com>
2023-05-10 21:45:05 -0400

    minor edits

--- website/src/components/About.jsx
+++ website/src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {HiArrowRight} from 'react-icons/hi';
 
 const About = () => {
     return (
@@ -12,11 +13,19 @@ const About = () => {
                     </div>
                 </div>
                 <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-                    <div>
+                    <div className='px-3'>
                         <p className='sm:text-right text-3xl font-bold'>Testting it's nice to meet you, jave a look around and learn some more</p>
                     </div>
-                    <div>
+                    <div className='px-3'>
                         <p>As a software engineer, I am dedicated to crafting elegant and efficient solutions to complex problems. With a strong attention to detail and a passion for learning, I am constantly seeking to improve my skills and stay up-to-date with the latest technologies. My collaborative mindset and strong communication skills make me a valuable team player who is always ready to contribute to the success of any project.</p>
+                        <div className='py-8'>
+                            <button className='text-amber-50 group border-2 px-3 py-1 flex hover:bg-pink-600 hover:scale-105 duration-300'>
+                                My Skills
+                                <span className='group-hover:rotate-90 duration-300 ml-2 my-1'>
+                            <HiArrowRight/>
+                        </span>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
